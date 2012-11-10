93339bf921a502413a2aaa02c259f77ad9b22fff
Tyler Gillies <tjgillies@gmail.com>
2012-11-10 11:12:57 -0800

    update key storage

--- bin/main.rb
+++ bin/main.rb
@@ -4,9 +4,13 @@ require_relative '../kam'
 # send FIND_* request to  alphas in parallel add responses to shortlist
 p "first line"
 p "bootstrapping: #{Kam.bootstrap}"
-Storage::DB.put Kam.sha1("monkey"), "testing"
+Kam::CONFIG["keys"].each do |key|
+  puts "Storing #{key}"
+  Storage::DB.put Kam.sha1(key), key
+end rescue nil
+
 puts "peers: #{Kam.peers}"
-puts "alphas: #{alphas = Kam.alphas(Kam.sha1("monkey"))}"
-puts "found nodes: #{found = Kam.find_value(alphas, Kam.sha1("monkey"))}"
+puts "alphas: #{alphas = Kam.alphas(Kam.sha1("charlie"))}"
+puts "found nodes: #{found = Kam.find_value(alphas, Kam.sha1("charlie "))}"
 #puts "closer nodes: #{Kam.find_value(found, Kam.sha1("monkey"))}"
 RpcServer.start
