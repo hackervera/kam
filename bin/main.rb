0a91f02f827b569ecf441c4289d89e796f84c889
Tyler Gillies <tjgillies@gmail.com>
2012-11-11 01:08:10 -0800

    remove leftover cruft

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,6 @@
 require_relative '../kam'
-# alphas are 3 contacts from kbucket
-# alphas make up 'shortlist'
-# send FIND_* request to  alphas in parallel add responses to shortlist
+
 p "bootstrapping: #{Kam.bootstrap}"
 p "peers: #{Kam.peers}"
-(Kam::CONFIG["files"] || []).each do |file|
-  f = File.read(file)
-  key = Kam.sha1(f)
-  puts "Storing #{key}"
-  Storage::DB.put key, f
-end
 
 WebServer.run
